e6bf0eff4057750c58ee783353cc3c9a9c711d91
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-03-15 16:40:27 +0100

    Initial Commit

---
+++ Permutations of Strings/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Permutations of Strings
+//
+//  Created by Kasia on 15/03/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
